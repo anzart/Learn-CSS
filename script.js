7eecadb015c1b49ae9e32f5c3b9ff6401f01848d
Abdenour TAKHERBOUCHT <codev.ta@gmail.com>
2019-06-11 10:09:28 +0200

    Test properties of 'dataset' in JS

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ const elem = document.querySelector("li:first-child");
 
 elem.dataset.id; // 10784
 elem.dataset.author; // Jason Walters
-elem.dataset.role = "Acteur de cinéma";
+elem.dataset.role = "Acteur de cinéma"; // // Renseigne le rôle de la personne
+"author" in elem.dataset; // true
+"notAttribute" in elem.dataset; // false
+elem.dataset.notAttribute = `Il n'ya pas d'attribut`; // Ajoute un nouvel attribut 'data' à l'élément
+"notAttribute" in elem.dataset; // true
