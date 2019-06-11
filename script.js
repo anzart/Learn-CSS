0d4694d4c146579acabe64415e00d197c28f1f35
Abdenour TAKHERBOUCHT <codev.ta@gmail.com>
2019-06-11 10:03:06 +0200

    Add JS Interaction with 'data-*' attribute

---
+++ script.js
@@ -0,0 +1,6 @@
+// Acces to data-* attribute value with "dataset":
+const elem = document.querySelector("li:first-child");
+
+elem.dataset.id; // 10784
+elem.dataset.author; // Jason Walters
+elem.dataset.role = "Acteur de cinéma";
